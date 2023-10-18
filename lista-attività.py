fd3cf16cc844c19f43466b8f0cb19257a7ea6354
albertocester96 <albertocester96@gmail.com>
2023-10-18 17:41:20 +0200

    check_empty

--- lista-attività.py
+++ lista-attività.py
@@ -1,24 +1,48 @@
-def aggiungi_attività(attività, lista):
-    lista.append(attività)
-    return lista
-
-def domanda(lista):
-    
+def check_empty(lista):
+    if not lista:
+        print ("La tua lista è vuota")
+     
+def aggiungi_attività(lista):
     attività_utente= input("Quale attività vuoi ricordare?: ")
 
     if attività_utente != "": 
-        aggiungi_attività(attività_utente, lista)
-        print(lista)
-        altro = input("Vuoi aggiungere altro? [Enter: si/no:]: ")
-       
-        if altro.lower() == "si":
-            domanda(lista)
-        else: 
-            exit
+        lista.append(attività_utente)
+        print("La tua lista è: ", lista)
+
     else:
-        print("Non hai indicato nessun valore, riprova")
-        domanda(lista)
+        print("Non hai inserito nessuna attività, prova a scrivere qualcosa")
+        aggiungi_attività(lista)
+        
     return lista
+
+def rimuovi_attività (lista):
+    check_empty(lista)
+    attività_da_rimuovere = input ("Quale attività vuoi rimuovere?: ")
+    lista.remove(attività_da_rimuovere)
+    print("Hai rimosso l'attività: "+ attività_da_rimuovere, ". La tua lista è " + lista)
+    
+    if not check_empty(lista):
+        print(lista)
+
+def domanda(lista):
+    
+    aggiungi_attività(lista)
+    
+    altro = input("Vuoi aggiungere altro? [Enter: si/no:]: ")
+       
+    if altro.lower() == "si":
+        check_empty(lista)
+        domanda(lista)
+    else: 
+        print("La tua lista è: ", lista)
+        rimuovi = input(". Vuoi rimuovere qualche attività?[si/no] ")
+            
+        if rimuovi.lower() == "si":
+            rimuovi_attività(lista)
+        elif rimuovi.lower() == "no":
+            exit
+    
+    
 
 lista_attività = [] 
 domanda(lista_attività)
