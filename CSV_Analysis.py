b7954ee9bdc147298e6a7d6f6c43053f951fb5e7
albertocester96 <albertocester96@gmail.com>
2023-10-24 17:47:02 +0200

    csv-import pd-import ic

--- CSV_Analysis.py
+++ CSV_Analysis.py
@@ -1,2 +1,18 @@
-#Questo script ha l'obiettivo di analizzare i dati CSV per fare delle somme, medie o rocercare valori specifici
+#Questo script ha l'obiettivo di analizzare i dati CSV per fare delle somme, medie o ricercare valori specifici
 
+
+import pandas as pd
+from icecream import ic 
+
+df = pd.read_csv("files/dolphin_insight_23102023_1756.csv", sep=";")
+
+header = df.columns #header del file
+
+colonna_strumento = df['valore']
+lista_presse=[]
+
+for riga in df.loc[df['strumento'].str.contains('Pressa', case=False)['strumeto']]:
+    lista_presse.append(riga)
+    
+
+ic(lista_presse)
