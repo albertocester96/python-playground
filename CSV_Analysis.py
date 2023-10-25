2f124e226957052b41ecb0e14ebdd5f2e0a6aff7
albertocester96 <albertocester96@gmail.com>
2023-10-25 07:18:14 +0200

    data-inizio-fine_print

--- CSV_Analysis.py
+++ CSV_Analysis.py
@@ -11,13 +11,16 @@ header = df.columns #header del file
 
 df['valore']= df['valore'].str.replace(",",".").astype(float).apply(math.ceil) #cambio "," su valori numerici con "." Converti in float e applica arrotondamento
 
-
+data_inizio= df.at[0, 'data inizio']
+data_fine= df.at[0, 'data_fine']
 ls_presse = df['strumento'].str.contains("Pressa",case= False) #filtro per parola "Pressa"
-ls_espansori = df['strumento'].str.contains("Espansori", case= False)
+ls_espansori = df['strumento'].str.contains("Espansori", case= False)#filtro per parola "Espansori"
+
 
 presse = df.loc[ls_presse] 
 espansori = df.loc[ls_espansori]
 
 somma_presse = presse['valore'].astype(float).apply(math.ceil).sum() #somma valori presse e arrotonda per eccesso
 
-ic("Le presse del reparto stampaggio hanno consumato " + str(somma_presse) + " kWh")
+print("Le presse del reparto stampaggio hanno consumato " + str(somma_presse) + " kWh, dal " + data_inizio + " al " + data_fine)
+
